a9246376c713156e55c080782d4104bb07d4b899
Arne Blankerts <Arne@Blankerts.de>
2014-11-27 21:04:19 +0100

    update pear package building shell script

--- pear.sh
+++ pear.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 rm -f fXSL*.tgz
 mkdir -p tmp/TheSeer/fXSL
+mkdir -p tmp/tests
 cp -r src/* tmp/TheSeer/fXSL
+cp -r tests/* tmp/tests
+cp package.xml phpunit.xml.dist LICENSE tmp
 phpab -o tmp/TheSeer/fXSL/autoload.php -b src src
-cp package.xml tmp
 cd tmp
 pear package
 mv fXSL*.tgz ..
